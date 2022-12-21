c35524d78d6faaa196adc1a21c3e6edf6731a96c
Đinh Huy Hoàng <20130265@st.hcmuaf.edu.vn>
2022-12-21 13:55:04 +0700

    fix

--- ShopThuCungMVC/Areas/Admin/Controllers/ProductController.cs
+++ ShopThuCungMVC/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using ShopThuCungMVC.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -17,7 +18,32 @@ namespace ShopThuCungMVC.Areas.Admin.Controllers
             else
                 return RedirectToAction("Login", "Auth");
         }
-
+        [HttpGet]
+        public ActionResult UploadFile()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            try
+            {
+                string _path = null;
+                if (file.ContentLength > 0)
+                {
+                    string _FileName = Path.GetFileName(file.FileName);
+                    _path = Path.Combine(Server.MapPath("~/Content/img/products"), _FileName);
+                    file.SaveAs(_path);
+                }
+                ViewBag.Message = "File Uploaded Successfully!!";
+                return Json(new { path =_path });
+            }
+            catch
+            {
+                ViewBag.Message = "File upload failed!!";
+                return Json(new { path = "Lỗi" });
+            }
+        }
         public ActionResult AddProduct()
         {
             UserAccount account = (UserAccount)Session["admin"];
